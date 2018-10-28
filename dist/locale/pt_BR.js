343cd6ad499f3aeba2acc457aa1eab48f1864392
wolflogin <44539529+wolflogin@users.noreply.github.com>
2018-10-28 03:00:37 -0300

    Create pt_BR.js
    Sugestão de tradução para a língua portuguesa.

---
+++ dist/locale/pt_BR.js
@@ -0,0 +1,10 @@
+{
+          TITLE   : 'HiddenColony',
+          API     : 'API',
+          USED_BY : 'Usado por',
+          LAS_MOD : 'Última atualização',
+          EXPIRES : 'Próxima atualização',
+          SER_TIM : 'Hora do servidor',
+          LOC_TIM : 'Hora local',
+          RES_TIM : 'Tempo restante'
+}     	
